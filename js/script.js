b6c2f64757b5b08ab4aebeadeb6d581db9c914be
AbdullahAzza1 <abdullahazza141@gmail.com>
2023-11-08 20:00:35 +0100

    step9

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ let numCelle = 100;
 let grid = document.getElementById("griglia");
 
 for (let i = 1; i <= numCelle; i++) {
-    
+
     let cella = creaQuadrato(i);
     grid.appendChild(cella);
 
@@ -14,11 +14,11 @@ function creaQuadrato(numero) {
     const cella = document.createElement("div");
     cella.classList.add("quadrati");
 
-    
-    cella.addEventListener("click", function() {
+
+    cella.addEventListener("click", function () {
 
         console.log("Cella cliccata", numero);
-        
+
         cella.classList.toggle("highlight");
 
     });
@@ -26,14 +26,14 @@ function creaQuadrato(numero) {
     return cella;
 }
 //creiamo dei numeri casuali
+let num = [1, 2, 3, 4, 5, 6, 7, 8, 10, 11, 12, 13, 14, 15, 16];
 
+for (let i = 0; i < grid.length; i++) {
 
-for (let i = 1; i < 16; i++) {
-    
-    num = Math.floor(Math.random() * 16);
+    grid.style
 
-    if (num == grid) {
-        console.log("cella giusta", num);
-    }
-    
+    console.log("cella giusta", num);
+
 }
+
+
