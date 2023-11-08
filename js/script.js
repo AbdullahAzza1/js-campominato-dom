8b742004b24941833cef1c4c8f77a450f9a51095
AbdullahAzza1 <abdullahazza141@gmail.com>
2023-11-08 16:58:45 +0100

    step7

--- js/script.js
+++ js/script.js
@@ -26,7 +26,10 @@ function creaQuadrato(numero) {
     return cella;
 }
 //creare dei numeri casuali
-function griglia() {
-    return Math.floor(Math.random() * 16);
-    document.getElementById("griglia").innerHTML ="Numero casuale " + numCelle;
-}
+//function griglia() {
+    //return Math.floor(Math.random() * 16);
+    //document.getElementById("griglia").innerHTML ="Numero casuale " + numCelle;
+//}
+
+let numCasuali = Math.floor(Math.random()*16)
+console.log(numCasuali);
